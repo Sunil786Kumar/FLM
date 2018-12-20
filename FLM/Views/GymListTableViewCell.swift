42bd728fdb01bb493e84a0d330e858db832eab39
Sunil Kumar <sunil.kumar@ladbrokes.com.au>
2018-12-20 11:30:56 +1000

    [ELM] project and pod commit

---
+++ FLM/Views/GymListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GymListTableViewCell.swift
+//  FLM
+//
+//  Created by Sunil Kumar on 20/12/18.
+//  Copyright © 2018 Sunil Kumar. All rights reserved.
+//
+
+import UIKit
+
+class GymListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-FLMTests/Pods-FLMTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FLMTests : NSObject
+@end
+@implementation PodsDummy_Pods_FLMTests
+@end
